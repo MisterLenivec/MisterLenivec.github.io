309072db0977c9dff6434aa84e8143fb8fc855db
Dmitry Sharomyshkin <Wormsom@Gmail.com>
2018-03-30 15:20:06 +0300

    git pages for 2 sites

---
+++ leonFullPage/js/slide.js
@@ -0,0 +1,33 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "flex";
+}
+
+function slideTime(n) {
+  n=1
+  showSlides(slideIndex += n);
+}
+
+setInterval(slideTime, 10000);﻿
