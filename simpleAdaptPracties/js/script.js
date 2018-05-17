c3224af022794ee6ae85cc3fcbcb2c01fc7ca837
Dmitry Sharomyshkin <Wormsom@Gmail.com>
2018-05-17 19:02:28 +0300

    third checkpoint

--- simpleAdaptPracties/js/script.js
+++ simpleAdaptPracties/js/script.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+	$(".item").magnificPopup({
+		type : 'image',
+		gallery : {
+			enabled : true
+		},
+		removalDelay: 300,
+		mainClass: 'mfp-fade'
+	});
+})
+
 menu.onclick = function myFunction() {
 	var x = document.getElementById("myTopnav");
 
