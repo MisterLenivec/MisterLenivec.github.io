c5b6dbe8433c9fdc5cfeccaf2dcb26c3d2ad4bec
Dmitry Sharomyshkin <Wormsom@Gmail.com>
2018-05-30 02:15:26 +0300

    updated script

--- simpleAdaptPracties/js/script.js
+++ simpleAdaptPracties/js/script.js
@@ -1,9 +1,15 @@
 menu.onclick = function myFunction() {
 	var x = document.getElementById("myTopnav");
+	var main = document.getElementsByTagName("main")[0];
 
 	if (x.className === "topnav") {
 		x.className += " responsive";
+		main.style.marginTop="290px";
 	} else {
 		x.className = "topnav";
+		main.style.marginTop="";
 	}
+
 }
+
+// main.addEventListener('click', addMarginTop);
